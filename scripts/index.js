cb6a47ccc9b07c72ae1391f78e9774dc0292077d
Nerovik <thegamingrelax19@gmail.com>
2022-04-02 15:57:56 +0700

    Initial commit

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+var button = document.getElementById('discord_btn');
+
+button.addEventListener("mouseover", function(){
+    button.textContent = "Nerovik#6528";
+});
+
+button.addEventListener("mouseout", function(){
+    button.textContent = "Discord";
+});

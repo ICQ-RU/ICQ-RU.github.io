619273b10356db84e99a40252e665b34079110c2
Agniya Kaneva <61701888+ICQ-RU@users.noreply.github.com>
2022-12-03 02:56:51 +0000

    changed nickname

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 var button = document.getElementById('discordBtn');
 
 button.addEventListener("mouseover", function(){
-    button.textContent = "Brizgalka#6528";
+    button.textContent = "ICQ#6528";
 });
 
 button.addEventListener("mouseout", function(){
